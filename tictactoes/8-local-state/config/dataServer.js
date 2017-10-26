13731f0914ab5aaa5b0b93429dd067ea342cf32e
colevandersWands <evan.t.cole@gmail.com>
2017-10-26 14:59:19 +0200

    Update dataServer.js

--- tictactoes/8-local-state/config/dataServer.js
+++ tictactoes/8-local-state/config/dataServer.js
@@ -1,18 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
-// cross origin resource sharing
-app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next){
   res.set('Access-Control-Allow-Origin' , '*');
-  res.set('Access-Control-Allow-Credentials', true);
-  res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
